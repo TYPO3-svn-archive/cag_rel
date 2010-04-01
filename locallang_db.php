714f9a9f3a876510ef9352877b65d5accbefef00
schrade <schrade@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-01 12:40:20 +0000

    Upload of tag versions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cag_rel/tags/0.3.3@31836 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ locallang_db.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Language labels for database tables/fields belonging to extension "cag_rel"
+ *
+ * This file is detected by the translation tool.
+ */
+
+$LOCAL_LANG = Array (
+	'default' => Array (
+		'pages.tx_cagrel_link' => 'Add page to relational links',
+		'pages.tx_cagrel_stdrel.I.0' => '',			
+		'pages.tx_cagrel_stdrel.I.1' => 'alternate',	
+		'pages.tx_cagrel_stdrel.I.2' => 'appendix',	
+		'pages.tx_cagrel_stdrel.I.3' => 'bookmark',	
+		'pages.tx_cagrel_stdrel.I.4' => 'chapter',	
+		'pages.tx_cagrel_stdrel.I.5' => 'contents',	
+		'pages.tx_cagrel_stdrel.I.6' => 'copyright',	
+		'pages.tx_cagrel_stdrel.I.7' => 'glossary',	
+		'pages.tx_cagrel_stdrel.I.8' => 'help',	
+		'pages.tx_cagrel_stdrel.I.9' => 'index',	
+		'pages.tx_cagrel_stdrel.I.10' => 'next',	
+		'pages.tx_cagrel_stdrel.I.11' => 'prev',	
+		'pages.tx_cagrel_stdrel.I.12' => 'section',	
+		'pages.tx_cagrel_stdrel.I.13' => 'start',	
+		'pages.tx_cagrel_stdrel.I.14' => 'stylesheet',	
+		'pages.tx_cagrel_stdrel.I.15' => 'subsection',			
+		'pages.tx_cagrel_stdrel' => 'Standard link relation',	
+		'pages.tx_cagrel_specrel' => 'Special link relation',	
+		'pages.tx_cagrel_params' => 'Additional link parameters (;)',	
+	),
+);
+?>
