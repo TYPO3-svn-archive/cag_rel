9b8737efd13a7fb621cb71622be7b9b6f2877042
schrade <schrade@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-01 13:15:44 +0000

    Version 0.3.3 released in TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cag_rel/trunk@31840 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "cag_rel"
+# Extension Manager/Repository config file for ext "cag_rel".
 #
-# Auto generated 22-04-2009 16:40
+# Auto generated 01-04-2010 15:13
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Generate relational link elements from pagerecords for the HEAD section of your HTML pages',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.3.2',
+	'version' => '0.3.3',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"0827";s:10:"README.txt";s:4:"95d7";s:8:"TODO.txt";s:4:"8cd5";s:12:"ext_icon.gif";s:4:"fc47";s:17:"ext_localconf.php";s:4:"1fd3";s:14:"ext_tables.php";s:4:"dcb5";s:14:"ext_tables.sql";s:4:"0f1f";s:16:"locallang_db.php";s:4:"6da5";s:14:"doc/manual.sxw";s:4:"3859";s:19:"doc/wizard_form.dat";s:4:"ead4";s:20:"doc/wizard_form.html";s:4:"b912";s:27:"pi1/class.tx_cagrel_pi1.php";s:4:"b93e";s:20:"static/constants.txt";s:4:"d41d";s:16:"static/setup.txt";s:4:"07ce";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"d5b6";s:10:"README.txt";s:4:"95d7";s:8:"TODO.txt";s:4:"8cd5";s:12:"ext_icon.gif";s:4:"fc47";s:17:"ext_localconf.php";s:4:"51ee";s:14:"ext_tables.php";s:4:"dcb5";s:14:"ext_tables.sql";s:4:"0f1f";s:16:"locallang_db.php";s:4:"6da5";s:14:"doc/manual.sxw";s:4:"7221";s:19:"doc/wizard_form.dat";s:4:"ead4";s:20:"doc/wizard_form.html";s:4:"b912";s:27:"pi1/class.tx_cagrel_pi1.php";s:4:"097b";s:16:"static/setup.txt";s:4:"fc77";}',
+	'suggests' => array(
+	),
 );
 
 ?>
